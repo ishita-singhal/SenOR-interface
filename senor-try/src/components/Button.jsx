d42e8f1abdbf62dbad933a73298473a1009d5f4f
Janhvi Singh <janhvi52@gmail.com>
2024-04-02 00:26:35 +0530

    added landing page

---
+++ senor-try/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div className='button_link'>
+        {/* <a href={CV} download>Resume</a> */}
+        <a href="https://enchatty-5tgbqv7ecfnupiwxgeteey.streamlit.app/" className='btn'>Let's have a Walkthrough</a>
+    </div>
+  )
+}
+
+export default Button
